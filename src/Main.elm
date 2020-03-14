27ffc1431b92e265e54318da8774b2f9fd778a3e
Dillon Kearns <dillon@dillonkearns.com>
2020-03-13 19:38:13 -0700

    Add store link.

--- src/Main.elm
+++ src/Main.elm
@@ -247,6 +247,8 @@ view siteMetadata page =
                                         [ text "Bio" ]
                                     , a [ class "block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4", Attr.href "#responsive-header" ]
                                         [ text "Music" ]
+                                    , a [ class "block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4", Attr.href "https://conner-cherland.square.site/", Attr.target "_blank" ]
+                                        [ text "Store" ]
                                     , a [ class "block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4", Attr.href "#responsive-header" ]
                                         [ text "Reviews" ]
                                     , a [ class "block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4", Attr.href (PagePath.toString Pages.pages.dates.index) ]
