26dc7bce4986050b3006c318efc3fa5a19ae49f3
Dillon Kearns <dillon@dillonkearns.com>
2021-07-29 16:47:53 -0700

    Add missing index.js.

---
+++ public/index.js
@@ -0,0 +1,11 @@
+/** @typedef {{load: (Promise<unknown>); flags: (unknown)}} ElmPagesInit */
+
+/** @type ElmPagesInit */
+export default {
+  load: async function (elmLoaded) {
+    const app = await elmLoaded;
+  },
+  flags: function () {
+    return "You can decode this in Shared.elm using Json.Decode.string!";
+  },
+};
