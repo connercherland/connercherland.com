3aaf3b1558fafec76779a17a6b8d3ebbb6222519
Dillon Kearns <dillon@dillonkearns.com>
2021-07-29 16:28:57 -0700

    2.0 checkpoint.

--- src/Shared.elm
+++ src/Shared.elm
@@ -1,120 +1,66 @@
-module Shared exposing (Model, Msg(..), PageView, RenderedBody, SharedMsg(..), StaticData, template)
+module Shared exposing (Data, Model, Msg(..), SharedMsg(..), template)
 
-import Css
-import Css.Global
-import Element exposing (Element)
+import Browser.Navigation
+import DataSource
 import Html exposing (Html)
-import Html.Styled as H exposing (div)
-import Html.Styled.Attributes as Attr exposing (css)
-import Icon
-import Pages
-import Pages.PagePath exposing (PagePath)
-import Pages.StaticHttp as StaticHttp
-import Tailwind.Utilities as Tw
-import TailwindView
-import TemplateType exposing (TemplateType)
+import Html.Styled
+import Pages.Flags
+import Pages.PageUrl exposing (PageUrl)
+import Path exposing (Path)
+import SharedTemplate exposing (SharedTemplate)
+import View exposing (View)
 
 
-type alias SharedTemplate templateDemuxMsg msg1 msg2 =
-    { init :
-        Maybe
-            { path :
-                { path : PagePath Pages.PathKey
-                , query : Maybe String
-                , fragment : Maybe String
-                }
-            , metadata : TemplateType
-            }
-        -> ( Model, Cmd Msg )
-    , update : Msg -> Model -> ( Model, Cmd Msg )
-    , view :
-        StaticData
-        ->
-            { path : PagePath Pages.PathKey
-            , frontmatter : TemplateType
-            }
-        -> Model
-        -> (Msg -> templateDemuxMsg)
-        -> PageView templateDemuxMsg
-        -> { body : Html templateDemuxMsg, title : String }
-    , map : (msg1 -> msg2) -> PageView msg1 -> PageView msg2
-    , staticData : List ( PagePath Pages.PathKey, TemplateType ) -> StaticHttp.Request StaticData
-    , subscriptions : TemplateType -> PagePath Pages.PathKey -> Model -> Sub Msg
-    , onPageChange :
-        Maybe
-            ({ path : PagePath Pages.PathKey
-             , query : Maybe String
-             , fragment : Maybe String
-             }
-             -> Msg
-            )
-    }
-
-
-template : SharedTemplate msg msg1 msg2
+template : SharedTemplate Msg Model Data SharedMsg msg
 template =
     { init = init
     , update = update
     , view = view
-    , map = map
-    , staticData = staticData
+    , data = data
     , subscriptions = subscriptions
     , onPageChange = Just OnPageChange
+    , sharedMsg = SharedMsg
     }
-
-
-type alias RenderedBody =
-    List (H.Html Never)
-
-
-type alias PageView msg =
-    { title : String, body : List (H.Html msg) }
 
 
 type Msg
     = OnPageChange
-        { path : PagePath Pages.PathKey
+        { path : Path
         , query : Maybe String
         , fragment : Maybe String
         }
     | SharedMsg SharedMsg
 
 
-type alias StaticData =
+type alias Data =
     ()
 
 
 type SharedMsg
-    = IncrementFromChild
+    = NoOp
 
 
 type alias Model =
     { showMobileMenu : Bool
-    , counter : Int
-    }
-
-
-map : (msg1 -> msg2) -> PageView msg1 -> PageView msg2
-map fn doc =
-    { title = doc.title
-    , body = List.map (H.map fn) doc.body
     }
 
 
 init :
-    Maybe
-        { path :
-            { path : PagePath Pages.PathKey
-            , query : Maybe String
-            , fragment : Maybe String
+    Maybe Browser.Navigation.Key
+    -> Pages.Flags.Flags
+    ->
+        Maybe
+            { path :
+                { path : Path
+                , query : Maybe String
+                , fragment : Maybe String
+                }
+            , metadata : route
+            , pageUrl : Maybe PageUrl
             }
-        , metadata : TemplateType
-        }
     -> ( Model, Cmd Msg )
-init maybePagePath =
-    ( { showMobileMenu = False
-      , counter = 0
-      }
+init navigationKey flags maybePagePath =
+    ( { showMobileMenu = False }
     , Cmd.none
     )
 
@@ -122,134 +68,34 @@ init maybePagePath =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        OnPageChange page ->
+        OnPageChange _ ->
             ( { model | showMobileMenu = False }, Cmd.none )
 
         SharedMsg globalMsg ->
-            case globalMsg of
-                IncrementFromChild ->
-                    ( { model | counter = model.counter + 1 }, Cmd.none )
+            ( model, Cmd.none )
 
 
-subscriptions : TemplateType -> PagePath Pages.PathKey -> Model -> Sub Msg
-subscriptions _ _ _ =
+subscriptions : Path -> Model -> Sub Msg
+subscriptions _ _ =
     Sub.none
 
 
-staticData : a -> StaticHttp.Request StaticData
-staticData siteMetadata =
-    StaticHttp.succeed ()
+data : DataSource.DataSource Data
+data =
+    DataSource.succeed ()
 
 
 view :
-    StaticData
+    Data
     ->
-        { path : PagePath Pages.PathKey
-        , frontmatter : TemplateType
+        { path : Path
+        , frontmatter : route
         }
     -> Model
     -> (Msg -> msg)
-    -> PageView msg
+    -> View msg
     -> { body : Html msg, title : String }
-view globalStaticData page model toMsg pageView =
-    { title = pageView.title
-    , body =
-        H.toUnstyled <|
-            div
-                [ css
-                    [ Tw.flex
-                    , Tw.flex_col
-                    , Tw.h_screen
-
-                    --, Tw.max_w_xl
-                    ]
-                ]
-                [ -- This will give us the standard tailwind style-reset as well as the fonts
-                  Css.Global.global Tw.globalStyles
-                , if page.path == Pages.pages.index then
-                    TailwindView.view
-
-                  else
-                    div
-                        [ css
-                            [ Tw.flex
-                            , Tw.flex_col
-                            , Tw.max_w_2xl
-                            , Tw.items_center
-                            , Tw.justify_center
-                            , Tw.justify_between
-                            , Tw.mx_auto
-                            , Tw.flex_grow
-                            ]
-                        ]
-                        pageView.body
-                , footer
-                ]
+view sharedData page model toMsg pageView =
+    { body = Html.Styled.toUnstyled (Html.Styled.div [] pageView.body)
+    , title = pageView.title
     }
-
-
-footer =
-    H.footer []
-        [ div []
-            [ div
-                [ css
-                    [ Tw.py_8
-                    , Tw.bg_darkGray
-                    , Tw.justify_center
-                    , Tw.flex
-                    ]
-                ]
-                [ div
-                    [ css
-                        [ Tw.flex
-                        , Tw.max_w_sm
-                        , Tw.w_full
-                        , Tw.justify_around
-                        ]
-                    ]
-                    ([ ( Icon.youtube, "https://www.youtube.com/user/itsconnercherland" )
-                     , ( Icon.spotify, "https://open.spotify.com/artist/33TOnR5uudaXvJjQhgNGk8" )
-                     , ( Icon.facebook, "https://facebook.com/connercherland/" )
-                     , ( Icon.instagram, "https://instagram.com/connercherland/" )
-                     , ( Icon.twitter, "https://twitter.com/ConnerCherland/" )
-                     ]
-                        |> List.map
-                            (\( icon, url ) ->
-                                div [ css [ Tw.w_8 ] ]
-                                    [ H.a
-                                        [ Attr.href url
-                                        , Attr.target "noopener"
-                                        ]
-                                        [ icon ]
-                                    ]
-                            )
-                    )
-                ]
-            , div
-                [ css
-                    [ Tw.flex
-                    , Tw.w_full
-                    , Tw.justify_center
-                    , Tw.py_8
-                    , Tw.bg_darkGray
-                    ]
-                ]
-                [ logo
-                ]
-            ]
-        ]
-
-
-logo =
-    H.img
-        [ Attr.src logoUrl
-        , css
-            [ Css.height (Css.px 160)
-            ]
-        ]
-        []
-
-
-logoUrl : String
-logoUrl =
-    "https://res.cloudinary.com/connercherland/image/upload/v1613279618/d2c8ce93487154e6ba130d9d590171ce_v155te.png"
--- src/Site.elm
+++ src/Site.elm
@@ -1,95 +1,42 @@
-module Site exposing (canonicalUrl, config, tagline)
+module Site exposing (config)
 
---import Cloudinary
-
-import Cloudinary
-import Color
-import MimeType
-import Pages exposing (images, pages)
-import Pages.ImagePath exposing (ImagePath)
+import DataSource
+import Head
 import Pages.Manifest as Manifest
-import Pages.Manifest.Category
-import Pages.PagePath exposing (PagePath)
-import Pages.StaticHttp as StaticHttp
-import TemplateType exposing (TemplateType)
+import Route
+import SiteConfig exposing (SiteConfig)
 
 
-type alias SiteConfig =
-    { canonicalUrl : String
-    , manifest : Manifest.Config Pages.PathKey
-    }
-
-
-config : SiteConfig
-config =
-    { canonicalUrl = canonicalUrl
-    , manifest = manifest
-    }
-
-
-type alias StaticData =
+type alias Data =
     ()
 
 
-staticData :
-    List ( PagePath Pages.PathKey, TemplateType )
-    -> StaticHttp.Request StaticData
-staticData siteMetadata =
-    StaticHttp.succeed ()
+config : SiteConfig Data
+config =
+    \_ ->
+        { data = data
+        , canonicalUrl = "https://elm-pages.com"
+        , manifest = manifest
+        , head = head
+        }
 
 
-canonicalUrl : String
-canonicalUrl =
-    "https://connercherland.netlify.app"
+data : DataSource.DataSource Data
+data =
+    DataSource.succeed ()
 
 
-manifest : Manifest.Config Pages.PathKey
-manifest =
-    { backgroundColor = Just Color.white
-    , categories = [ Pages.Manifest.Category.education ]
-    , displayMode = Manifest.Standalone
-    , orientation = Manifest.Portrait
-    , description = "Conner Cherland Music"
-    , iarcRatingId = Nothing
-    , name = "Conner Cherland Music"
-    , themeColor = Just Color.white
-    , startUrl = pages.index
-    , shortName = Just "Conner Cherland"
-    , sourceIcon = images.iconPng
-    , icons =
-        [ icon webp 192
-        , icon webp 512
-        , icon MimeType.Png 192
-        , icon MimeType.Png 512
-        ]
-    }
+head : Data -> List Head.Tag
+head static =
+    [ Head.sitemapLink "/sitemap.xml"
+    ]
 
 
-tagline : String
-tagline =
-    "Conner Cherland Music"
-
-
-webp : MimeType.MimeImage
-webp =
-    MimeType.OtherImage "webp"
-
-
-icon :
-    MimeType.MimeImage
-    -> Int
-    -> Manifest.Icon pathKey
-icon format width =
-    { src = cloudinaryIcon format width
-    , sizes = [ ( width, width ) ]
-    , mimeType = format |> Just
-    , purposes = [ Manifest.IconPurposeAny, Manifest.IconPurposeMaskable ]
-    }
-
-
-cloudinaryIcon :
-    MimeType.MimeImage
-    -> Int
-    -> ImagePath pathKey
-cloudinaryIcon mimeType width =
-    Cloudinary.urlSquare "v1613279618/d2c8ce93487154e6ba130d9d590171ce_v155te.png" (Just mimeType) width
+manifest : Data -> Manifest.Config
+manifest static =
+    Manifest.init
+        { name = "Site Name"
+        , description = "Description"
+        , startUrl = Route.Index {} |> Route.toPath
+        , icons = []
+        }
